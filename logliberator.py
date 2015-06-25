cd6192bc31ac01150e90acda0aabf91082e16b7e
Shawn Jones <shawnjones20@gmail.com>
2015-06-25 17:18:30 -0400

    Initial commit

---
+++ logliberator.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import unicodecsv
+import requests
+from bs4 import BeautifulSoup as soup
+
+print"**********************************************"
+print"*******     Logbook Liberator v0.1     *******"
+print"**********************************************"
+print""
+
+_username = raw_input("Username: ")
+_password = raw_input("Password: ")
+_maxPagesStr = raw_input("Pages: ")
+
+_maxPages = int(_maxPagesStr)
+
+f = open('Logbook.csv', 'w')
+
+payload = {
+	'username': _username,
+	'password': _password
+	}
+
+print""
+print"Here we go. Viva la Logbook!!"
+print""
+	
+with requests.Session() as s:
+	p = s.post('https://www.qrz.com/login', data=payload)
+	#r = s.get('http://logbook.qrz.com')
+
+for i in range(1, _maxPages+1):
+    print"Working on page: %s" % i
+    getpages = {'page': i }
+    r = s.post('http://logbook.qrz.com', data=getpages)		
+    data = soup(r.text)
+    writer = unicodecsv.writer(f)
+    
+    table = data.findAll('tr', {'class':'lrow'})
+    for i in range(0, len(table)):
+        sheet = []
+        for row in table[i].findAll("td", {'class':['lpos', 'ldt', 'ltm', 'lde', 'lba', 'lfr', 'lmo', 'lgr', 'lcc', 'lop']}):
+            x = row.text
+            sheet.append(x)
+            
+        writer.writerows([sheet])
+f.close()
+
+print""
+print"Logbook liberated!"
+print""
